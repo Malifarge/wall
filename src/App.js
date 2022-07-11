f7a275b83e1d4f95ab904e424f0d6b1569f9553e
Thomas <malifarge.thomas9@gmail.com>
2022-07-11 17:24:58 +0200

    card as affiched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import users from './users.json'
 import Card from './components/Card'
+import './styles/global.css'
 
 class App extends React.Component {
   render() {
     return (
-      // hello
+      <main>
+        <h1>The wall</h1>
+        <section>
+        {users.map((user, key ) => (
+          <Card key={key} picture={user.picture} name={user.name} about={user.about}/>
+    ))}
+        </section>
+        
+      </main>
     )
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 class Card extends React.Component {
   render() {
+    console.log(this);
     return (
-      // hello
+      <article className='box col-sm-3 col-6'>
+        <div className='profil'>
+            <div className='PP'>
+              <img source={this.props.picture} alt="128 x 128"/>
+            </div>
+            <h2>{this.props.name}</h2>
+        </div>
+
+        <div>
+            <p>{this.props.about}</p>
+        </div>
+      </article>
     )
   }
 }
